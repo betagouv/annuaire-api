21ebc12658ca82285a9b5b64f9e0fe5a6d4304b2
Thomas Guillet <guillet.thomas@gmail.com>
2018-02-19 17:36:30 +0100

    Improve test robustness
    * Won't fail when new files are created

--- test/enrich.test.js
+++ test/enrich.test.js
@@ -22,18 +22,18 @@ describe('enrich', function () {
   })
 
   it('adds organisme in dataset', () => {
-    assert.equal(Object.keys(dataset.organismes).length, 1)
-    assert.ok(dataset.organismes.cdas_brest_bellevue.properties, 1)
+    assert.ok(Object.keys(dataset.organismes).length >= 1)
+    assert.ok(dataset.organismes.cdas_brest_bellevue.properties)
   })
 
   it('adds the organisme ID in commune', () => {
-    assert.equal(Object.keys(dataset.communes['29011'].organismes).length, 1)
-    assert.equal(dataset.communes['29011'].organismes.cdas.length, 1)
-    assert.equal(dataset.communes['29011'].organismes.cdas[0], 'cdas_brest_bellevue')
+    assert.ok(Object.keys(dataset.communes['29011'].organismes).length >= 1)
+    assert.ok(dataset.communes['29011'].organismes.cdas.length >= 1)
+    assert.ok(dataset.communes['29011'].organismes.cdas.indexOf('cdas_brest_bellevue') >= 0)
   })
 
   it('adds an organisme in departement', () => {
-    assert.equal(Object.keys(dataset.departements['29'].organismes).length, 1)
+    assert.ok(Object.keys(dataset.departements['29'].organismes).length >= 1)
     assert.ok(dataset.departements['29'].organismes.cdas[0].properties)
   })
 })
