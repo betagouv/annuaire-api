2de533c01a426ddf52539fd28a59bdb7ecbb5f13
Thomas Guillet <guillet.thomas@gmail.com>
2018-10-26 12:06:55 +0200

    Load NVM in deployment script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,16 @@ cd $(dirname "$BASH_SOURCE")
 
 set -ev
 
+# Loads NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Get latest master
 git pull
+
+# Update packages
 yarn
+
+# Restart server
 /home/cloud/.nvm/versions/node/v6.12.2/bin/pm2 reload annuaire_v3
