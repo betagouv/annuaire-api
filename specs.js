f1d7386f76e51f68660cc4f0855621f09d14080a
Thomas Guillet <guillet.thomas@gmail.com>
2019-04-29 09:22:21 +0200

    Met à jour les specs de l'API

--- specs.js
+++ specs.js
@@ -7,6 +7,15 @@ const labels = Object.keys(types).map(function (key) {
 })
 const description = ["Code du type de l'organisme :", ...labels].join('\n')
 
+const responses = {
+  '200': {
+    'description': 'Liste des organismes du département'
+  },
+  '404': {
+    'description': 'Département introuvable'
+  }
+}
+
 const specs = {
   'swagger': '2.0',
   'info': {
@@ -21,17 +30,17 @@ const specs = {
     'application/json'
   ],
   'paths': {
-    '/v1/organismes/{departement}/{type}': {
+    '/v3/departements/{departement}/{type}': {
       'get': {
         'summary': 'Trouver des organismes par département et par type',
         'tags': [
-          'Organismes'
+          'Organismes', 'Département'
         ],
         'parameters': [
           {
             'name': 'departement',
             'in': 'path',
-            'description': 'Code departement sur deux caractères',
+            'description': 'Code département sur deux caractères',
             'required': true,
             'type': 'string',
             'default': '14'
@@ -46,14 +55,65 @@ const specs = {
             'description': description
           }
         ],
-        'responses': {
-          '200': {
-            'description': 'Liste des organismes du département'
+        'responses': responses
+      }
+    },
+    '/v3/communes/{commune}/{type}': {
+      'get': {
+        'summary': 'Trouver des organismes par commune et par type',
+        'tags': [
+          'Organismes', 'Commune'
+        ],
+        'parameters': [
+          {
+            'name': 'commune',
+            'in': 'path',
+            'description': 'Code commune sur cinq caractères',
+            'required': true,
+            'type': 'string',
+            'default': '27103'
           },
-          '404': {
-            'description': 'Département introuvable'
+          {
+            'name': 'type',
+            'in': 'path',
+            'required': true,
+            'type': 'string',
+            'default': 'mairie',
+            'enum': Object.keys(types),
+            'description': description
           }
-        }
+        ],
+        'responses': responses
+      }
+    },
+    '/v1/organismes/{departement}/{type}': {
+      'get': {
+        'summary': 'Trouver des organismes par département et par type',
+        'description': 'Les données ne sont plus mises à jour.',
+        'tags': [
+          'Organismes', 'Département'
+        ],
+        'deprecated': true,
+        'parameters': [
+          {
+            'name': 'departement',
+            'in': 'path',
+            'description': 'Code département sur deux caractères',
+            'required': true,
+            'type': 'string',
+            'default': '14'
+          },
+          {
+            'name': 'type',
+            'in': 'path',
+            'required': true,
+            'type': 'string',
+            'default': 'maison_handicapees',
+            'enum': Object.keys(types),
+            'description': description
+          }
+        ],
+        'responses': responses
       }
     }
   }
