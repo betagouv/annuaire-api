3bfe6d9b0c32483056f2087cbf476f0645d65518
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-11-28 19:10:12 +0100

    Improve build script exit on error

--- build.js
+++ build.js
@@ -3,8 +3,7 @@ const { build } = require('./main')
 const fileName = 'all_latest.tar.bz2'
 const url = `http://lecomarquage.service-public.fr/donnees_locales_v2/${fileName}`
 
-build(url, fileName)
-  .catch(err => {
-    console.error(err)
-    process.exitCode = 1
-  })
+build(url, fileName).catch(err => {
+  console.error(err)
+  process.exit(1)
+})
