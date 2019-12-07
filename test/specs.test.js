28adaf7ccab66d194e0d5ff096fa9c0863bf8085
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-12-08 00:36:35 +0100

    Remove obsolete test

--- test/specs.test.js
+++ test/specs.test.js
@@ -1,7 +1,6 @@
 const fs = require('fs')
 const yaml = require('js-yaml')
 
-const { additions } = require('../build/additions')
 const definitions = yaml.safeLoad(fs.readFileSync('definitions.yaml'))
 const types = yaml.safeLoad(fs.readFileSync('types.yaml'))
 const assert = require('assert')
@@ -15,22 +14,6 @@ describe('specs', function () {
 
       testedTypes.forEach(type => {
         assert(typeIds.indexOf(type) >= 0, type)
-      })
-    })
-  })
-
-  describe('script', function () {
-    it('should refers to reference types', function () {
-      additions.forEach(addition => {
-        if (addition.type) {
-          assert(typeIds.indexOf(addition.type) >= 0, addition.type)
-        } else if (addition.types) {
-          addition.types.forEach(type => {
-            assert(typeIds.indexOf(type) >= 0, type)
-          })
-        } else {
-          assert(false)
-        }
       })
     })
   })
