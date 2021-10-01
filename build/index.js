3a218c4ba94e0605d90aac1ba0d0521f9671d651
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-10-01 14:13:45 +0200

    Remove additional sources

--- build/index.js
+++ build/index.js
@@ -2,39 +2,9 @@ const { join } = require('path')
 const { generateInitialDataset } = require('./initial')
 const { writeJsonArray } = require('./util')
 
-const additions = [
-  // 'sources/cotes-d-armor',
-  'sources/haute-garonne',
-  // 'sources/hauts-de-seine',
-  'sources/metropole-lyon',
-  'sources/saone-et-loire',
-  'sources/seine-et-marne',
-  'sources/seine-saint-denis',
-  'data-directory'
-]
-
-async function computeAdditionalOrganismes (specificSource, config) {
-  const organismes = []
-  const sources = specificSource ? [specificSource] : additions
-
-  await Promise.all(sources.map(async addition => {
-    const computedOrganismes = await require('./' + addition).computeOrganismes(config)
-    organismes.push(...computedOrganismes)
-    console.log(`${addition} : ajout de ${computedOrganismes.length} organismes`)
-  }))
-
-  return organismes
-}
-
 async function build () {
-  if (process.argv.length > 2) {
-    const organismes = await computeAdditionalOrganismes(process.argv[2], process.argv[3])
-    await writeJsonArray(join(__dirname, '..', 'dataset.json'), organismes)
-  } else {
-    const initialOrganismes = await generateInitialDataset()
-    const additionalOrganismes = await computeAdditionalOrganismes()
-    await writeJsonArray(join(__dirname, '..', 'dataset.json'), [...initialOrganismes, ...additionalOrganismes])
-  }
+  const initialOrganismes = await generateInitialDataset()
+  await writeJsonArray(join(__dirname, '..', 'dataset.json'), initialOrganismes)
 }
 
 build().catch(err => {
--- build/util.js
+++ build/util.js
@@ -1,8 +1,7 @@
-const { writeFile, readFile } = require('fs').promises
+const { writeFile } = require('fs').promises
 
 const { keyBy } = require('lodash')
 const communes = require('@etalab/decoupage-administratif/data/communes.json')
-const yaml = require('js-yaml')
 
 const communesIndex = keyBy(communes, 'code')
 
@@ -11,19 +10,9 @@ async function writeJsonArray (filePath, data) {
   await writeFile(filePath, str, { encoding: 'utf-8' })
 }
 
-async function readYaml (filePath) {
-  const content = await readFile(filePath, { encoding: 'utf-8' })
-  return yaml.safeLoad(content)
-}
-
 function expandCommune (codeCommune) {
   const commune = communesIndex[codeCommune]
   return commune ? `${codeCommune} ${commune.nom}` : `${codeCommune} ???`
 }
 
-function getCommunes (codeDepartement) {
-  const communesDepartement = communes.filter(c => c.type === 'commune-actuelle' && c.departement === codeDepartement)
-  return communesDepartement.map(c => expandCommune(c.code))
-}
-
-module.exports = { writeJsonArray, readYaml, expandCommune, getCommunes }
+module.exports = { writeJsonArray, expandCommune }
